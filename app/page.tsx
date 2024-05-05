af7cbee5b9b99d914158104518f7f670ba16819f
manoranjith shankar <ma.ran4073@gmail.com>
2024-05-05 14:25:06 +0530

    [update] pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import FilePicker from "@/components/FilePicker";
-import { Button, Card, CardBody, CardFooter, CardHeader, Image } from "@nextui-org/react"
 
 export default function Home() {
 	return (
@@ -7,23 +6,6 @@ export default function Home() {
 			<div className="inline-block max-w-lg text-center justify-center">
         <h1 className="text-2xl font-bold mb-3">Connect your wallet and mint NFTs!</h1>
 		    <FilePicker />
-	    {/* <p className="m-3">OR</p>
-      <Card className="py-4">
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <h4 className="font-bold text-large">Mint BYC NFTs</h4>
-      </CardHeader>
-      <CardBody className="py-2">
-        <Image
-          alt="Card background"
-          className="object-cover rounded-xl"
-          src="https://nextui.org/images/hero-card-complete.jpeg"
-          width={400}
-        />
-      </CardBody>
-	  <CardFooter className="justify-center">
-		<Button className="w-full">Mint NFT</Button>
-	  </CardFooter>
-    </Card> */}
 			</div>
 		</section>
 	);
